5777b3e5a9aa773d2d3cf0b1135a1b84f2b42f05
fathur <fathur.jtk09@gmail.com>
2022-01-29 15:12:09 +0700

    add image

--- coba_chuy/lib/main.dart
+++ coba_chuy/lib/main.dart
@@ -4,9 +4,15 @@ void main() {
   runApp(
       MaterialApp(
         home: Scaffold(
+          backgroundColor: Colors.blueGrey,
           appBar: AppBar(
             title: Text("Ole GUN"),
             backgroundColor: Colors.blueGrey[800],
+          ),
+          body: Center(
+            child: Image(
+              image: NetworkImage("https://i.pinimg.com/564x/b6/1a/b0/b61ab0d6f9701059245fcc02f554fbae.jpg"),
+            ),
           ),
         ),
       ),
